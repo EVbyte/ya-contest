dd9b0adc92026b9bf35fa27c911509abacebfe52
Anton <kr1an@hotmail.com>
2017-04-15 18:04:17 +0300

    shortest-way/feature: add initial structure

---
+++ shortest-way/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+void init(){
+}
+
+int main()
+{
+    // get product max product of: two max, two min values
+    init();
+    return 0;
+}
