c14fe6e05976e81e75ecafa5a15dc493898877d6
Anton <kr1an@hotmail.com>
2017-04-15 18:01:14 +0300

    common-problem/feature: add final OK version

--- common-problem/main.cpp
+++ common-problem/main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
-#include <cstdlib> // для system
+#include <algorithm>
+
 using namespace std;
 
-int main() 
-{ 
-    cout << "Hello, world!" << endl;
-    system("pause"); // Только для тех, у кого MS Visual Studio
-    return 0; 
+
+int size;
+long long arr[int(1e5) + 10];
+
+
+void init(){
+    cin >> size;
+    for(int i = 0; i < size; i++)
+        cin >> arr[i];
+}
+
+int main()
+{
+    // get product max product of: two max, two min values
+    init();
+    sort(arr, arr + size);
+    cout << max(arr[0] * arr[1], arr[size-1] * arr[size-2]);
+    return 0;
 }
