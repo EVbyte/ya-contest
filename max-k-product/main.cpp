6b89e214258d08e93e6d586c8206c302b22cda13
Anton <kr1an@hotmail.com>
2017-04-15 22:56:39 +0300

    max-k-product/feature: add initial structure

---
+++ max-k-product/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+const int LMAX = int(1e5) + 10;
+const int NMAX = int(1e9)*2 + 10;
+
+long long A[LMAX];
+int N
+int K;
+
+void input()
+{
+    cin >> N >> K;
+    for(int i=0; i<N; i++)
+        cin >> A[i];
+}
+
+void init()
+{
+    input();
+}
+
+void calc()
+{
+
+}
+
+int main()
+{
+    init();
+    calc();
+
+    return 0;
+}
