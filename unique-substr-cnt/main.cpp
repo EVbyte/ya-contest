81ef686f7f6e3f45f312514775fd103693c0064b
Anton <kr1an@hotmail.com>
2017-04-16 18:25:21 +0300

    unique-substr-cnt/feature: add base structure. do not know how to solve

---
+++ unique-substr-cnt/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+const int MAX = int(1e6)*2 + 10;
+
+string S;
+long long Q;
+int strs[MAX];
+int endss[MAX];
+int roof;
+
+
+void init(){
+}
+
+int main()
+{
+    init();
+}
