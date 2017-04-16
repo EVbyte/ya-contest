95220860586c35774068c57c84182f7321b88187
Anton <kr1an@hotmail.com>
2017-04-16 18:38:24 +0300

    tree-sum/feature: basic structure

---
+++ tree-sum/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <utility>
+
+using namespace std;
+
+const int MAX = int(1e6)*2 + 10;
+
+
+string S;
+long long Q;
+int strs[MAX];
+int endss[MAX];
+int roof;
+
+
+void init(){
+}
+
+int main()
+{
+    pair<int, int> m=  new pair<int, int>({1, 1});
+    cout << m.first;
+    init();
+}
