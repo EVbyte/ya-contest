99774dca36da965ea0c4c0e46bad5a5ca499da9b
Anton <kr1an@hotmail.com>
2017-04-25 21:18:50 +0300

    knight-movements/feature: add ok status version

--- knight-movements/main.cpp
+++ knight-movements/main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 const int LMAX = int(1e2) + 10;
 const int NMAX = int(1e5);
 
+int t = 1;
 int n = 0;
 int m = 0;
 int i = 0;
@@ -18,14 +19,14 @@ void dfs (int i, int j, int scnt) {
     if(scnt < A[i][j]){
         A[i][j] = scnt;
         if(i-1 >= 0 && j-2 >= 0) dfs(i-1, j-2, scnt+1);
-        if(i-1 >= 0 && j+2 < n)  dfs(i-1, j+2, scnt+1);
+        if(i-1 >= 0 && j+2 <= m) dfs(i-1, j+2, scnt+1);
         if(i-2 >= 0 && j-1 >= 0) dfs(i-2, j-1, scnt+1);
-        if(i-2 >= 0 && j+1 < n)  dfs(i-2, j+1, scnt+1);
+        if(i-2 >= 0 && j+1 <= m) dfs(i-2, j+1, scnt+1);
 
-        if(i+1 < m && j-2 >= 0)  dfs(i+1, j-2, scnt+1);
-        if(i+1 < m && j+2 < n)   dfs(i+1, j+2, scnt+1);
-        if(i+2 < m && j-1 >= 0)  dfs(i+2, j-1, scnt+1);
-        if(i+2 < m && j+1 < n)   dfs(i+2, j+1, scnt+1);
+        if(i+1 <= n && j-2 >= 0) dfs(i+1, j-2, scnt+1);
+        if(i+1 <= n && j+2 <= m) dfs(i+1, j+2, scnt+1);
+        if(i+2 <= n && j-1 >= 0) dfs(i+2, j-1, scnt+1);
+        if(i+2 <= n && j+1 <= m) dfs(i+2, j+1, scnt+1);
     }
 }
 
@@ -33,8 +34,10 @@ void init(){
     cin >> n >> m >> i >> j;
     i -= 1;
     j -= 1;
-    for(int k = 0; k < n; k++){
-        for(int v = 0; v < m; v++){
+    n -= 1;
+    m -= 1;
+    for(int k = 0; k <= n; k++){
+        for(int v = 0; v <= m; v++){
             A[k][v] = NMAX;
         }
     }
@@ -43,9 +46,9 @@ void init(){
 int main()
 {
     init();
-    dfs(n-1, 0, 0);
+    dfs(0, 0, 0);
     if(A[i][j] == NMAX)
-        cout << "NEVER";
+        cout << "NEVAR";
     else
         cout << A[i][j];
     // count << (A[i][j] == NMAX ? "NEVER" : A[i][j]);
